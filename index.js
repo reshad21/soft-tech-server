33a41df58856361395a0c74a3654e5f28b29a165
reshad21 <miakala7@gmail.com>
2023-01-05 13:46:50 +0600

    mongo db added tothe project

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -7,12 +8,16 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
- 
- 
+
 app.get('/',(req,res)=>{
     res.send('server is running');
 })
  
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gplljg9.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
  
 app.listen(port,()=>{
     console.log(`Listening to the port ${port}`);
